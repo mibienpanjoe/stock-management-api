47f54fce4681d58001e169271512a04fe27ccf14
mibienpanjoe <epixprint00@gmail.com>
2026-01-06 15:51:33 +0000

    refactor project structure

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const express = require('express');
-
-const app = express();
+const app = require('./src/app');
 
 const PORT = 3000;
-
-app.get('/api/health', (req, res) => { res.status(200).json({
-status: 'API is running'});
-});
 
 app.listen(PORT, () => {
 console.log('Server running on port ' + PORT); });
